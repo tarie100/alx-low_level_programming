0cff5487043d5b364765cadacbc90892c687e46c
tarie100 <tarie.school@gmail.com>
2023-06-12 12:27:32 -0700

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include"main.h"
+#include<string.h>
+/**
+ * _strdup - returns a copy of given string
+ * @str: given string
+ * Return: pointer to duplicate string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int p = strlen(str);
+
+	ptr = malloc(sizeof(char) * (p + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(ptr, str);
+	return (ptr);
+}
