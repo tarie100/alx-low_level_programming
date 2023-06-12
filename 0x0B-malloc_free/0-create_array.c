35221ed943bac61e2a42d042428edd86023ee95d
tarie100 <tarie.school@gmail.com>
2023-06-12 04:30:29 -0700

    0_create-array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
 
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL || size == 0)
 	{
 		return (NULL);
 	}
-	*ptr = c;
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = c;
+	}
 	return (ptr);
 }
