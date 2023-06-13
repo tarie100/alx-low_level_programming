8bd3564b23bf579656c1f7210da504fab2df28bf
tarie100 <tarie.school@gmail.com>
2023-06-13 04:00:12 -0700

    4-free_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,11 +9,13 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL || height == 0)
+	{
+		return;
+	}
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
-		grid[i] = NULL;
 	}
-	free(*grid);
-	grid = NULL;
+	free(grid);
 }
