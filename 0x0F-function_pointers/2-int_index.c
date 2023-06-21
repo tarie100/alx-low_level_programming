edd7e4714d8ef69981820815bfaef2b697b1defa
tarie100 <tarie.school@gmail.com>
2023-06-21 12:32:40 -0700

    2-int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include<stddef.h>
+#include"function_pointers.h"
+/**
+ * int_index - searches for an int
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
