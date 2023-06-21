9e59fc07144cbf64f9cec8b7976f4e947691ae71
tarie100 <tarie.school@gmail.com>
2023-06-21 08:27:51 -0700

    0-name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include"function_pointers.h"
+#include<stddef.h>
 /**
  * print_name - prints a name
  * @name: name
@@ -6,5 +7,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 	f(name);
 }
