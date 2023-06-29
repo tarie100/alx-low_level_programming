8a4b4a9a16b20c5464acd3c90b9faf0332775177
tarie100 <tarie.school@gmail.com>
2023-06-29 06:54:21 -0700

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t a = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str != NULL)
+		if (h->str)
 		{
-			printf("[%d]%s\n", h->len, h->str);
+			printf("[%d]%s\n");
 		}
 		else
 		{
-			printf("[0] (nill)\n");
+			printf("[0] (nill)\n", h->len, h->str);
 		}
 		h = h->next;
 		a++;
