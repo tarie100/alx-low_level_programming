2aedd33ac325bd06ea4f16d3797581868d4c9c51
tarie100 <tarie.school@gmail.com>
2023-07-04 23:57:43 -0700

    5-free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include"lists.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: ..
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while ((current = *head) == NULL)
+	{
+		free(current);
+	}
+}
