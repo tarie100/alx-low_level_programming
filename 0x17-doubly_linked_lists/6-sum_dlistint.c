5965751f72b8a8d4d488ea55c6c80544943b1804
tarie100 <tarie.school@gmail.com>
2023-09-14 13:17:35 -0700

    fix betty

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,12 @@
 #include"lists.h"
 /**
- * sum_dlistint - returns the sum 
+ * sum_dlistint - returns the sum
  * @head: 1st node
  * Return: 0
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum =0;
+	int sum = 0;
 
 	while (head != NULL)
 	{
