0d17ad03ab8b30e263447748fc90b8faaf498830
tarie100 <tarie.school@gmail.com>
2023-09-18 12:31:24 -0700

    dynamic

---
+++ 0x18-dynamic_libraries/puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * puts - outputs string
+ * @str: pointer to string
+ * * main - Entry point
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
